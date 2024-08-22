ee142d1385839d00e8df82aa41e84f639e8a4907
Wanessa <w.c.n.@hotmail.com>
2024-08-21 21:40:33 -0300

    alteracao3

--- Python/controle/controle_visitantes/urls.py
+++ Python/controle/controle_visitantes/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from usuarios.views import index
+from visitantes.views import registrar_visitante
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name="usuarios"),
+    path('registrar-visitante/', registrar_visitante, name="registrar_visitante"),
 ]
--- Python/controle/visitantes/views.py
+++ Python/controle/visitantes/views.py
@@ -1,1 +1,7 @@
 
+from django.shortcuts import render
+
+def registrar_visitante(request):
+    context = {}
+    
+    return render(request, "Telacadastrovisitantes.html", context)
